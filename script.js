4dc14e1f1c0ae478632c9459984420b57cb47968
durbonca <durbonca@gmail.com>
2020-07-28 09:10:04 -0400

    finish logic menu

--- script.js
+++ script.js
@@ -3,49 +3,52 @@ var locations = document.getElementById("locations")
 var faq = document.getElementById("faq")
 
 document.getElementById("menu_1").addEventListener("click", function() {
-    if ((home.classList.contains("show"))) {
+    if (home.classList.contains("show")) {
         console.log("do nothing")
     } else {
-        console.log("do something")
         home.classList.add("show");
         home.classList.remove("hide");
-
+        if (faq.classList.contains("show")) {
+            faq.classList.add("hide");
+            faq.classList.remove("show");
+        } else {
+            locations.classList.add("hide");
+            locations.classList.remove("show");
+        }
 
     }
-    /*   home.classList.add("show");
-    home.classList.remove("show");
-    faq.classList.add("hide");
-    faq.classList.remove("hide");
-    locations.classList.add("hide");
-    locations.classList.remove("hide"); */
 })
 
 document.getElementById("menu_2").addEventListener("click", function() {
-    if ((locations.classList.contains("show"))) {
+    if (locations.classList.contains("show")) {
         console.log("do nothing")
     } else {
-        console.log("do something")
         locations.classList.add("show");
         locations.classList.remove("hide");
-
+        if (home.classList.contains("show")) {
+            home.classList.add("hide");
+            home.classList.remove("show");
+        } else {
+            locations.classList.add("hide");
+            locations.classList.remove("show");
+        }
     }
-    /*   home.classList.add("show");
-    home.classList.remove("show");
-    faq.classList.add("hide");
-    faq.classList.remove("hide");
-    locations.classList.add("hide");
-    locations.classList.remove("hide"); */
 })
 document.getElementById("menu_3").addEventListener("click", function() {
-    if ((faq.classList.contains("show"))) {
+    if (faq.classList.contains("show")) {
         console.log("do nothing")
     } else {
-        console.log("do something")
+        faq.classList.add("show");
+        faq.classList.remove("hide");
+        if (home.classList.contains("show")) {
+            home.classList.add("hide");
+            home.classList.remove("show");
+        } else {
+            locations.classList.add("hide");
+            locations.classList.remove("show");
+        }
+
+
     }
-    /*   home.classList.add("show");
-    home.classList.remove("show");
-    faq.classList.add("hide");
-    faq.classList.remove("hide");
-    locations.classList.add("hide");
-    locations.classList.remove("hide"); */
+
 })
